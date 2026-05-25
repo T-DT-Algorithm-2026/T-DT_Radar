1343df8054cbd1d71225e877784a142d6507551e
liu <3978818034@qq.com>
2026-05-25 23:57:50 +0800

    分区赛4

--- src/tdt_vision/launch/radar_fly.launch.py
+++ src/tdt_vision/launch/radar_fly.launch.py
@@ -1,48 +1,32 @@
 from launch import LaunchDescription
-from launch.actions import TimerAction
-from launch_ros.actions import LoadComposableNodes
-from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
 
-    def get_radar_detect_fly_node(package, plugin):
-        return ComposableNode(
+    def get_radar_detect_fly_node(package, executable):
+        return Node(
             package=package,
-            plugin=plugin,
+            executable=executable,
             name='radar_detect_fly_node',
-            extra_arguments=[{'use_intra_process_comms': True}]
+            output='screen',
+            emulate_tty=True,
         )
 
-    def get_lock_node(package, plugin):
-        return ComposableNode(
+    def get_lock_node(package, executable):
+        return Node(
             package=package,
-            plugin=plugin,
+            executable=executable,
             name='lock_node',
-            extra_arguments=[{'use_intra_process_comms': True}]
-        )
-
-    def get_radar_loader(radar_detect_fly_node, lock_node):
-        return TimerAction(
-            period=1.0,
-            actions=[
-                LoadComposableNodes(
-                    target_container='camera_detector_container',
-                    composable_node_descriptions=[
-                        #变向设置启动顺序
-                        radar_detect_fly_node,
-                        lock_node,
-                    ],
-                )
-            ],
+            output='screen',
+            emulate_tty=True,
         )
 
     # 创建节点描述
-    radar_detect_fly_node = get_radar_detect_fly_node('tdt_vision', 'tdt_radar::DetectFly')
-    lock_node = get_lock_node('tdt_lock', 'tdt_lock::Lock')
+    radar_detect_fly_node = get_radar_detect_fly_node('tdt_vision', 'radar_detect_fly_node')
+    lock_node = get_lock_node('tdt_lock', 'lock_node')
 
-    # 加载到已有的相机检测容器中
-    radar_loader = get_radar_loader(radar_detect_fly_node, lock_node)
     return LaunchDescription([
-            radar_loader,
+            radar_detect_fly_node,
+            lock_node,
         ])
